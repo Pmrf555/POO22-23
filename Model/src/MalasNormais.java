8a1e27594d787c8a12019a0a8c0bbe28bf09356a
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-07 15:15:40 +0100

    Create MalasNormais.java

---
+++ Model/src/MalasNormais.java
@@ -0,0 +1,18 @@
+import java.time.LocalDateTime;
+
+public class MalasNormais extends Malas{
+
+    public MalasNormais(int numeroUtilizadores, Double estado, String descricao, String marca
+            , int codigoAlfa, Double precoBase, Double correcaoPreco, Double desconto, int dimensao, String material, int ano) {
+        super(numeroUtilizadores, estado, descricao, marca, codigoAlfa, precoBase, correcaoPreco, desconto, dimensao, material, ano);
+    }
+
+    public MalasNormais(Artigos art, int dimensao, String material, int ano) {
+        super(art, dimensao, material, ano);
+    }
+
+    public double preco(){
+        return getPrecoBase() - (4 * LocalDateTime.now().getYear()-getAno()); //o valor decresce 4 euros por ano, o enunciado não diz nada de jeito aqui
+
+    }
+}
