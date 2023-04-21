d946004ae8a063bfee3a25ad35e9eb3ce292ab5b
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-21 14:19:18 +0100

    Create SapatilhasPremium.java

---
+++ src/Model/SapatilhasPremium.java
@@ -0,0 +1,15 @@
+package Model;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class SapatilhasPremium extends Sapatilhas{ //nunca tem desconto mas aumenta o preço consoante os anos que ele tem
+
+    public SapatilhasPremium(Artigos artigos,Sapatilhas sapatilhas){
+        super(artigos,sapatilhas);
+    }
+
+    public Double preco(){
+        return super.getPrecoBase() + (10 * LocalDateTime.now().until(super.getDataLancamento(), ChronoUnit.YEARS)); // o 10 foi inventado não sei se é preciso um número especifico
+    }
+}
