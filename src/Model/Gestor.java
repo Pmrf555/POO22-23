ad5632ec64e254d483b250e72d852f03db13b140
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-21 14:19:02 +0100

    Create Gestor.java

---
+++ src/Model/Gestor.java
@@ -0,0 +1,12 @@
+package Model;
+
+import java.util.Map;
+
+public class Gestor {
+    private Map<Long,Artigos> artigosMap; //key->codigo alfanumerico
+    private Map<String ,Transportadora> transportadoraMap; // key-> nome
+    private Map<Integer,Utilizador> utilizadorMap; //key->codigo
+    private Map<Integer,Encomendas> encomendasMap; //key->numero de encomenda (variável de classe que incrementa sempre que se cria uma encomenda
+
+
+}
