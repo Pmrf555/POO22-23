472e290377f79381c17ae38f1e2413a60ba967a7
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-21 14:19:16 +0100

    Create SapatilhasNormais.java

---
+++ src/Model/SapatilhasNormais.java
@@ -0,0 +1,18 @@
+package Model;
+
+import java.time.LocalDateTime;
+
+public class SapatilhasNormais extends Sapatilhas{
+    public SapatilhasNormais(Artigos artigos,Sapatilhas sapatilhas){
+        super(artigos,sapatilhas);
+    }
+
+    public Double preco(){
+        return getPrecoBase() + (getPrecoBase() / getNumeroUtilizadores() * getEstado()) * (getDesconto() / 100);
+    }
+
+    public void desconto(Double desconto){
+        if(getDataLancamento().getYear() != LocalDateTime.now().getYear() || getTamanho() > 45) setDesconto(desconto);
+        else setDesconto(1.0);
+    }
+}
