1273ae9b2ad1468777d09df0c59e784eaf1b1329
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-21 15:54:44 +0100

    Update MalasPremium.java

--- src/Model/MalasPremium.java
+++ src/Model/MalasPremium.java
@@ -5,6 +5,10 @@ import java.time.LocalDateTime;
 public class MalasPremium extends Malas{
     private double valorizacao;
 
+    public MalasPremium() {
+
+    }
+
     public double getValorizacao() {
         return valorizacao;
     }
@@ -17,7 +21,7 @@ public class MalasPremium extends Malas{
         return getPrecoBase() + (this.valorizacao * LocalDateTime.now().getYear()-getAno());
     }
 
-    public MalasPremium(int numeroUtilizadores, Double estado, String descricao, String marca, int codigoAlfa
+    public MalasPremium(int numeroUtilizadores, Double estado, String descricao, String marca, Long codigoAlfa
             , Double precoBase, Double correcaoPreco, Double desconto, int dimensao, String material, int ano, double valorizacao) {
         super(numeroUtilizadores, estado, descricao, marca, codigoAlfa, precoBase, correcaoPreco, desconto, dimensao, material, ano);
         this.valorizacao = valorizacao;
