576feb9b5dead0b17f815808be3f9576d873c05a
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-21 15:54:56 +0100

    Update SapatilhasNormais.java

--- src/Model/SapatilhasNormais.java
+++ src/Model/SapatilhasNormais.java
@@ -7,6 +7,12 @@ public class SapatilhasNormais extends Sapatilhas{
         super(artigos,sapatilhas);
     }
 
+    public SapatilhasNormais(int numeroUtilizadores, Double estado, String descricao, String marca, Long codigoAlfa
+            , Double precoBase, Double correcaoPreco, Double tamanho, Boolean atacadores, String cor
+            , LocalDateTime dataLancamento, Double desconto) {
+        super(numeroUtilizadores, estado, descricao, marca, codigoAlfa, precoBase, correcaoPreco, tamanho, atacadores, cor, dataLancamento, desconto);
+    }
+
     public Double preco(){
         return getPrecoBase() + (getPrecoBase() / getNumeroUtilizadores() * getEstado()) * (getDesconto() / 100);
     }
