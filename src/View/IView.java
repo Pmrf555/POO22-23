36ed99c1822a290f2f05c7069ef9cf60b9e54edc
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-26 00:03:07 +0100

    Update IView.java

--- src/View/IView.java
+++ src/View/IView.java
@@ -1,8 +1,15 @@
 package View;
 
+import Controller.IInput;
+
 public interface IView {
 
     void mostrarMenuPrincipal();
-
+    public void mostraMenuArtigos();
+    public static void pressEnterToContinue(IInput input){
+        System.out.println(" --- press enter ---");
+        input.InputString();
+    }
+    public void mostraMensagem(String msg);
     public static void clearScreen() {}
 }
