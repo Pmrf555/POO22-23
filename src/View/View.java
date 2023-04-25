87af2e276cc62df9ac45866cf0cffae0f37bb9ab
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-26 00:03:10 +0100

    Update View.java

--- src/View/View.java
+++ src/View/View.java
@@ -1,18 +1,19 @@
 package View;
 
+import Controller.IInput;
+
 public class View implements IView{
 
-    public View(){
-    }
+    public View() { }
 
     public void mostrarMenuPrincipal(){
         clearScreen();
         Menu menu = new Menu();
         menu.setTitulo("marketplace Vintage \uD83C\uDFEA");
-        menu.adicionaOpcao("Adicionar Artigo");
+        menu.adicionaOpcao("Adicionar Artigo \uD83D\uDCE5");
         menu.adicionaOpcao("Adicionar Transportadora \uD83D\uDE9B");
         menu.adicionaOpcao("Adicionar Encomenda \uD83D\uDCE6");
-        menu.adicionaOpcao("Adicionar Utilizador");
+        menu.adicionaOpcao("Adicionar Utilizador \uD83D\uDCE6");
         menu.adicionaOpcao("Sair ❌");
         menu.show(true);
     }
@@ -21,19 +22,21 @@ public class View implements IView{
         clearScreen();
         Menu menu = new Menu();
         menu.setTitulo("marketplace Vintage \uD83C\uDFEA");
-        menu.adicionaOpcao("Adicionar Artigo");
-        menu.adicionaOpcao("Adicionar Transportadora \uD83D\uDE9B");
-        menu.adicionaOpcao("Adicionar Encomenda \uD83D\uDCE6");
-        menu.adicionaOpcao("Adicionar Utilizador");
+        menu.adicionaOpcao("Adicionar Sapatilha Normal \uD83D\uDC5F");
+        menu.adicionaOpcao("Adicionar Sapatilha Premium \uD83D\uDC5E");
+        menu.adicionaOpcao("Adicionar Mala Normal \uD83D\uDC5B");
+        menu.adicionaOpcao("Adicionar Mala Premium \uD83D\uDC5C");
+        menu.adicionaOpcao("Adicionar T-Shirt \uD83D\uDC55");
         menu.adicionaOpcao("Sair ❌");
         menu.show(true);
     }
 
+    public void mostraMensagem(String msg){
+        System.out.println(msg);
+    }
 
 
-    /**
-     * Limpar o ecrã
-     */
+
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
