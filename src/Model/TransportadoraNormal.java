bedd508b4a624ffa295693842bc54b52dee3b39a
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-25 21:43:40 +0100

    Update TransportadoraNormal.java

--- src/Model/TransportadoraNormal.java
+++ src/Model/TransportadoraNormal.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 public class TransportadoraNormal extends Transportadora{
     public TransportadoraNormal(Transportadora transportadora) {super (transportadora);}
 
+
     public TransportadoraNormal(String nome, Double precoBasePequena, Double precoBaseMedia,
                                 Double precoBaseGrande, Double imposto, Double formula){
         super(nome, precoBasePequena, precoBaseMedia, precoBaseGrande, imposto, formula);
