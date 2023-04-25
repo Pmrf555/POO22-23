45bc7b24abe89a1cd4f9711a285a3fcfd0d2978d
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-26 00:02:58 +0100

    Update Controller.java

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -24,6 +24,18 @@ public class Controller {
             int escolha = input.InputInteger();
             switch (escolha) {
                 case 1:
+                    adicionaArtigo(view,input,gestor);
+                    break;
+                case 2:
+                    adicionaTransportadora(view,input,gestor);
+                    break;
+                case 3:
+                    adicionaEncomenda(view,input,gestor);
+                    break;
+                case 4:
+                    adicionaUtilizador(view,input,gestor);
+                    break;
+                case 5:
                     input.closeScanner();
                     System.exit(0);
                     break;
@@ -31,6 +43,66 @@ public class Controller {
                     break;
             }
         }
+    }
+
+    public static void adicionaArtigo(IView view,IInput input,IGestor gestor){
+        view.mostraMenuArtigos();
+        int escolha = input.InputInteger();
+        switch (escolha){
+            case 1:
+                adicionaSapatilhaNormal(view,input,gestor);
+                break;
+            case 2:
+                adicionaSapatilhaPremium(view,input,gestor);
+                break;
+            case 3:
+                adicionaMalaNormal(view,input,gestor);
+                break;
+            case 4:
+                adicionaMalaPremium(view,input,gestor);
+                break;
+            case 5:
+                adicionaTShirt(view,input,gestor);
+                break;
+            case 6:
+                return;
+            default:
+                view.mostraMensagem("Erro na escolha do artigo");
+                IView.pressEnterToContinue(input);
+                break;
+        }
+    }
+
+    public static void adicionaSapatilhaNormal(IView view,IInput input,IGestor gestor){
+
+    }
+
+    public static void adicionaSapatilhaPremium(IView view,IInput input,IGestor gestor){
+
+    }
+
+    public static void adicionaMalaNormal(IView view,IInput input,IGestor gestor){
+
+    }
+
+    public static void adicionaMalaPremium(IView view,IInput input,IGestor gestor){
+
+    }
+
+    public static void adicionaTShirt(IView view,IInput input,IGestor gestor){
+
+    }
+
+    public static void adicionaTransportadora(IView view,IInput input,IGestor gestor){
+
+    }
+
+    public static void adicionaEncomenda(IView view,IInput input,IGestor gestor){
+
+    }
+
+    public static void adicionaUtilizador(IView view,IInput input,IGestor gestor){
+
     }
 
 }
