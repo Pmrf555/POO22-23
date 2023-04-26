92a8bdbac4a9afeae32e5d36778e8898819ad6f3
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-27 00:18:18 +0100

    Update SapatilhasPremium.java

--- src/Model/SapatilhasPremium.java
+++ src/Model/SapatilhasPremium.java
@@ -2,22 +2,18 @@ package Model;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 public class SapatilhasPremium extends Sapatilhas{ //nunca tem desconto mas aumenta o preço consoante os anos que ele tem
 
-    public SapatilhasPremium(int numeroUtilizadores, Double estado, String descricao, String marca, Long codigoAlfa, Double precoBase, Double correcaoPreco, Double tamanho, Boolean atacadores, String cor, LocalDateTime dataLancamento) {
-        super(numeroUtilizadores, estado, descricao, marca, codigoAlfa, precoBase, correcaoPreco, tamanho, atacadores, cor, dataLancamento);
-    }
-
-    public SapatilhasPremium(int numeroUtilizadores, Double estado, String descricao, String marca, Double precoBase, Double correcaoPreco, Double tamanho, Boolean atacadores, String cor, LocalDateTime dataLancamento) {
+    public SapatilhasPremium(int numeroUtilizadores, Double estado, String descricao, String marca, Double precoBase
+            , Double correcaoPreco, Double tamanho, Boolean atacadores, String cor, Date dataLancamento) {
         super(numeroUtilizadores, estado, descricao, marca, precoBase, correcaoPreco, tamanho, atacadores, cor, dataLancamento);
-    }
-    public SapatilhasPremium(Artigos artigos, Sapatilhas sapatilhas){
-        super(artigos,sapatilhas);
     }
 
     public Double preco(){
-        return super.getPrecoBase() + (10 * LocalDateTime.now().until(super.getDataLancamento(), ChronoUnit.YEARS)); // o 10 foi inventado não sei se é preciso um número especifico
+        Date now = new Date();
+        return super.getPrecoBase() + (10 * (now.getYear() - super.getDataLancamento().getYear())); // o 10 foi inventado não sei se é preciso um número especifico
     }
 
     public String toString(){
