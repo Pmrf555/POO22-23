800052eb5bf33b220452daea80cb0001833b9025
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-26 17:42:05 +0100

    Update MalasPremium.java

--- src/Model/MalasPremium.java
+++ src/Model/MalasPremium.java
@@ -27,8 +27,15 @@ public class MalasPremium extends Malas{
         this.valorizacao = valorizacao;
     }
 
-    public MalasPremium(Artigos art, int dimensao, String material, int ano, double valorizacao) {
-        super(art, dimensao, material, ano);
+    public MalasPremium(int numeroUtilizadores, Double estado, String descricao, String marca
+            , Double precoBase, Double correcaoPreco, int dimensao, String material, int ano, double valorizacao) {
+        super(numeroUtilizadores, estado, descricao, marca, precoBase, correcaoPreco, dimensao, material, ano);
         this.valorizacao = valorizacao;
+    }
+    public String toString(){
+        return "Número Utilizadores: " + getNumeroUtilizadores() + "\nEstado: "
+                +getEstado()+ "\nDescricao: " +getDescricao()+ "\nMarca: "+getMarca()+ "\nCodigo Alfanumerico: "
+                +getCodigoAlfa()+"\nPreço Base: "+getPrecoBase()+"\nCorreção Preço: "+getCorrecaoPreco()+"\nDimensão: "
+                +getDimensao()+ "\nMaterial: " +getMaterial()+"\nAno: "+getAno()+"\nValorização: "+getValorizacao();
     }
 }
