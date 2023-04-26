5b5042ba174c71ef36d9aefe31137c153d77224d
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-26 17:42:19 +0100

    Update SapatilhasNormais.java

--- src/Model/SapatilhasNormais.java
+++ src/Model/SapatilhasNormais.java
@@ -12,7 +12,11 @@ public class SapatilhasNormais extends Sapatilhas{
             , LocalDateTime dataLancamento) {
         super(numeroUtilizadores, estado, descricao, marca, codigoAlfa, precoBase, correcaoPreco, tamanho, atacadores, cor, dataLancamento);
     }
-
+    public SapatilhasNormais(int numeroUtilizadores, Double estado, String descricao, String marca
+            , Double precoBase, Double correcaoPreco, Double tamanho, Boolean atacadores, String cor
+            , LocalDateTime dataLancamento) {
+        super(numeroUtilizadores, estado, descricao, marca, precoBase, correcaoPreco, tamanho, atacadores, cor, dataLancamento);
+    }
     public Double preco(){
         return getPrecoBase() + (getPrecoBase() / getNumeroUtilizadores() * getEstado()) * (getCorrecaoPreco() / 100);
     }
@@ -20,5 +24,11 @@ public class SapatilhasNormais extends Sapatilhas{
     public void desconto(Double desconto){
         if(getNumeroUtilizadores() > 0 || getTamanho() > 45) setCorrecaoPreco(desconto);
         else setCorrecaoPreco(1.0);
+    }
+    public String toString(){
+        return "Número Utilizadores: " + getNumeroUtilizadores() + "\nEstado: "
+                +getEstado()+ "\nDescricao: " +getDescricao()+ "\nMarca: "+getMarca()+ "\nCodigo Alfanumerico: "
+                +getCodigoAlfa()+"\nPreço Base: "+getPrecoBase()+"\nCorreção Preço: "+getCorrecaoPreco()+"\nTamanho: "
+                +getTamanho()+ "\nAtacadores: " +getAtacadores()+"\nCor: "+getCor();
     }
 }
