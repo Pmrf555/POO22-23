35d690154fe796e3591fba61e063ed371aa97da9
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-26 14:05:14 +0100

    Update Controller.java

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,14 +1,20 @@
 package Controller;
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Formatter;
+import java.util.List;
 
 import Model.*;
 import View.*;
 
 public class Controller {
 
-    public static void run() throws ClassNotFoundException, IOException {
+    public static void run() throws ClassNotFoundException, IOException, ParseException {
         IView view = new View();
         IInput input = new Input();
         IGestor gestor = new Gestor();
@@ -51,13 +57,13 @@ public class Controller {
         int escolha = input.InputInteger();
         switch (escolha){
             case 1:
-                adicionaSapatilha(view,input,gestor);
+                adicionarSapatilhaNormal(view,input,gestor);
                 break;
             case 2:
-                adicionaSapatilha(view,input,gestor);
+                adicionarSapatilhaPremium(view,input,gestor);
                 break;
             case 3:
-                adicionaMala(view,input,gestor);
+                adicionaMalaNormal(view,input,gestor);
                 break;
             case 4:
                 adicionaMalaPremium(view,input,gestor);
@@ -106,42 +112,147 @@ public class Controller {
         gestor.getArtigosMap().put(codAlfa,new SapatilhasNormais(numeroUtilizadores,estado,descricao,marca,codAlfa,precoBase,correcaoPreco,dimensao,atacadores,cor,data1));
     }
 
-    public static void adicionaSapatilha(IView view,IInput input,IGestor gestor){
-
+    public static void adicionarSapatilhaPremium(IView view,IInput input,IGestor gestor){
+        view.mostraMensagem("Insira o número de utilizadores:");
+        Integer numeroUtilizadores = input.InputInteger();
+        view.mostraMensagem("Insira estado:");
+        Double estado = input.InputDouble();
+        view.mostraMensagem("Insira uma descrição:");
+        String descricao = input.InputString();
+        view.mostraMensagem("Insira o nome da marca:");
+        String marca = input.InputString();
+        view.mostraMensagem("Insira código alfanumérico:");
+        Long codAlfa = input.InputLong();
+        view.mostraMensagem("Insira preço base:");
+        Double precoBase = input.InputDouble();
+        view.mostraMensagem("Insira correção do preço");
+        Double correcaoPreco = input.InputDouble();
+        view.mostraMensagem("Insira o tamanho:");
+        Double dimensao = input.InputDouble();
+        view.mostraMensagem("0- Não tem atacadores:\n1-Tem atacadores");
+        Boolean atacadores;
+        if(input.InputInteger() == 0){
+            atacadores = false;
+        }else {
+            atacadores = true;
+        }
+        view.mostraMensagem("Insira a cor:");
+        String cor = input.InputString();
+        view.mostraMensagem("Insira a data de lançamento(formato dd/MM/yyy)");
+        String data = input.InputString();
+        LocalDateTime data1 = LocalDateTime.parse(data);
+        gestor.getArtigosMap().put(codAlfa,new SapatilhasNormais(numeroUtilizadores,estado,descricao,marca,codAlfa,precoBase,correcaoPreco,dimensao,atacadores,cor,data1));
     }
     // numeroUtilizadores, estado, descricao, marca, codigoAlfa, precoBase, correcaoPreco, desconto, dimensao, material, ano
-    public static void adicionaMala(IView view,IInput input,IGestor gestor){
-        view.mostraMensagem("Insira a dimensão do produto:");
+    public static void adicionaMalaNormal(IView view,IInput input,IGestor gestor){
+        view.mostraMensagem("Insira o número de utilizadores:");
+        Integer numeroUtilizadores = input.InputInteger();
+        view.mostraMensagem("Insira estado:");
+        Double estado = input.InputDouble();
+        view.mostraMensagem("Insira uma descrição:");
+        String descricao = input.InputString();
+        view.mostraMensagem("Insira o nome da marca:");
+        String marca = input.InputString();
+        view.mostraMensagem("Insira código alfanumérico:");
+        Long codAlfa = input.InputLong();
+        view.mostraMensagem("Insira preço base:");
+        Double precoBase = input.InputDouble();
+        view.mostraMensagem("Insira correção do preço");
+        Double correcaoPreco = input.InputDouble();
+        view.mostraMensagem("Insira o tamanho:");
         Integer dimensao = input.InputInteger();
         view.mostraMensagem("Insira o tipo de material:");
         String material = input.InputString();
         view.mostraMensagem("Insira o ano:");
         Integer ano = input.InputInteger();
+        gestor.getArtigosMap().put(codAlfa,new MalasNormais(numeroUtilizadores,estado,descricao,marca,codAlfa,precoBase,correcaoPreco,dimensao,material,ano));
     }
 
     public static void adicionaMalaPremium(IView view,IInput input,IGestor gestor){
-        view.mostraMensagem("Insira a valorização da mala:");
+        view.mostraMensagem("Insira o número de utilizadores:");
+        Integer numeroUtilizadores = input.InputInteger();
+        view.mostraMensagem("Insira estado:");
+        Double estado = input.InputDouble();
+        view.mostraMensagem("Insira uma descrição:");
+        String descricao = input.InputString();
+        view.mostraMensagem("Insira o nome da marca:");
+        String marca = input.InputString();
+        view.mostraMensagem("Insira código alfanumérico:");
+        Long codAlfa = input.InputLong();
+        view.mostraMensagem("Insira preço base:");
+        Double precoBase = input.InputDouble();
+        view.mostraMensagem("Insira correção do preço");
+        Double correcaoPreco = input.InputDouble();
+        view.mostraMensagem("Insira o tamanho:");
+        Integer dimensao = input.InputInteger();
+        view.mostraMensagem("Insira o tipo de material:");
+        String material = input.InputString();
+        view.mostraMensagem("Insira o ano:");
+        Integer ano = input.InputInteger();
+        view.mostraMensagem("Insira a valorização:");
         Double valorizacao = input.InputDouble();
+        gestor.getArtigosMap().put(codAlfa,new MalasPremium(numeroUtilizadores,estado,descricao,marca,codAlfa,precoBase,correcaoPreco,dimensao,material,ano,valorizacao));
     }
 
     public static void adicionaTShirt(IView view,IInput input,IGestor gestor){
+        view.mostraMensagem("Insira o número de utilizadores:");
+        Integer numeroUtilizadores = input.InputInteger();
+        view.mostraMensagem("Insira estado:");
+        Double estado = input.InputDouble();
+        view.mostraMensagem("Insira uma descrição:");
+        String descricao = input.InputString();
+        view.mostraMensagem("Insira o nome da marca:");
+        String marca = input.InputString();
+        view.mostraMensagem("Insira código alfanumérico:");
+        Long codAlfa = input.InputLong();
+        view.mostraMensagem("Insira preço base:");
+        Double precoBase = input.InputDouble();
+        view.mostraMensagem("Insira correção do preço");
+        Double correcaoPreco = input.InputDouble();
         view.mostraMensagem("Insira o tamanho:");
         String tamanho = input.InputString();
         view.mostraMensagem("Insira o padrão:");
         String padrao = input.InputString();
+        gestor.getArtigosMap().put(codAlfa,new TShirt(numeroUtilizadores,estado,descricao,marca,codAlfa,precoBase,correcaoPreco,tamanho,padrao));
     }
 
     public static void adicionaTransportadora(IView view,IInput input,IGestor gestor){
         view.mostraMensagem("Insira o nome:");
         String nome = input.InputString();
         view.mostraMensagem("Insira a margem de lucro:");
-        Double padrao = input.InputDouble();
+        Double margemLucro = input.InputDouble();
+        gestor.getTransportadoraMap().put(nome,new Transportadora(nome,margemLucro));
     }
 
-    public static void adicionaEncomenda(IView view,IInput input,IGestor gestor){
+    public static void adicionaEncomenda(IView view,IInput input,IGestor gestor) throws ParseException {
+        view.mostraMensagem("Quantos artigos vai ter a encomenda?");
+        Integer numeroArtigos = input.InputInteger();
+        view.mostraMensagem("Agora insira os códigos alfanuméricos dos" +numeroArtigos+ " artigos:");
+        List<Artigos> codAlfa = new ArrayList<Artigos>();
+        for (int i = 0; i<numeroArtigos;i++){
+            Long aux = input.InputLong();
+            codAlfa.add(gestor.getArtigosMap().get(aux));
+        }
         view.mostraMensagem("Insira a dimensão da embalagem:");
         String dimensaoEmbalagem = input.InputString();
-        view.mostraMensagem("Insira ");
+        view.mostraMensagem("Insira o preco final:");
+        Double precoFinal = input.InputDouble(); // preço final tem de ser calculado e não perguntado ao user
+        view.mostraMensagem("Insira a taxa de satisfação de serviço novo:");
+        Double satisfacaoServicoNovo = input.InputDouble();
+        view.mostraMensagem("Insira a taxa de satisfação de serviço usado:");
+        Double satisfacaoServicoUsado = input.InputDouble();
+        view.mostraMensagem("Insirao custoExpedicao:"); // deve ser calculada por nós também
+        Double custoExpedicao = input.InputDouble();
+        view.mostraMensagem("Insira o estado:");
+        String estado = input.InputString();
+        view.mostraMensagem("Insira a data de criação:");
+        String dataCriacao = input.InputString();
+        view.mostraMensagem("Insira o prazo limite:");
+        String prazoLimite = input.InputString();
+        Date dataCriacao1,prazoLimite1;
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        dataCriacao1 = formatter.parse(dataCriacao);
+        prazoLimite1 = formatter.parse(prazoLimite);
     }
 
     public static void adicionaUtilizador(IView view,IInput input,IGestor gestor){
