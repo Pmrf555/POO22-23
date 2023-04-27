ed861d5d6aba8ecbd5e5b174e093e2b2a33ca6a3
Pedro Ferreira <sm8epedro18@gmail.com>
2023-04-27 14:58:10 +0100

    Update IGestor.java

--- src/Model/IGestor.java
+++ src/Model/IGestor.java
@@ -2,10 +2,15 @@ package Model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 public interface IGestor extends Serializable {
     public Utilizador vendedorQueMaisFaturouSempre();
+    public double lucroVintage();
+    public List<Utilizador> maioresVendedoresSistema(Date inicio,Date fim, int quantosNoTop);
+    public List<Utilizador> maioresCompradoresSistema(Date inicio,Date fim, int quantosNoTop);
+    public List<Encomendas> encomendasEmitidasPorVendedor(Integer codUtilizador);
     public Utilizador vendedorQueMaisFaturouEntreDatas(Date inicio, Date fim);
     public Map<Long, Artigos> getArtigosMap();
     public Transportadora transportadoraComMaiorFaturacao();
