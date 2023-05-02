c2373e68f54835e8f54964ecd33431264aa4c61f
Pedro Ferreira <sm8epedro18@gmail.com>
2023-05-02 18:39:11 +0100

    Update Malas.java

--- src/Model/Malas.java
+++ src/Model/Malas.java
@@ -1,9 +1,21 @@
 package Model;
 
+import java.util.Objects;
+
 public abstract class Malas extends Artigos{
     private int dimensao;
     private String material;
     private int ano;
+
+    /*
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Malas malas = (Malas) o;
+        return dimensao == malas.getDimensao() && ano == malas.getAno() && material.equals(malas.getMaterial());
+    }*/
+
 
     public Malas(int numeroUtilizadores, Double estado, String descricao, String marca
             , Double precoBase, Double correcaoPreco, int dimensao, String material, int ano) {
