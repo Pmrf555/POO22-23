183194c9383857eb8b5c80330401987541662fde
Pedro Ferreira <sm8epedro18@gmail.com>
2023-05-02 18:39:05 +0100

    Update IView.java

--- src/View/IView.java
+++ src/View/IView.java
@@ -4,6 +4,10 @@ import Controller.IInput;
 
 public interface IView {
     public void mostrarMenuSimulacao();
+
+    public void mostrarMenuUser();
+    public void mostraMenuPrincipalUser();
+    public void mostraMenuPrincipalAdmin();
     void mostrarMenuPrincipal();
     public void mostraMenuArtigos();
     public void pressEnterToContinue(IInput input);
